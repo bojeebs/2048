6b7299f484cbe9aa5199e9ed6c65f6d01c6e999c
Tyler Carter <113203952+bojeebs@users.noreply.github.com>
2023-02-03 12:36:57 -0600

    test

---
+++ script.js
@@ -0,0 +1,25 @@
+// Global Variables
+
+// various variables for ids from HTML 
+//If i use a 2d array I think i need to set a row and col variable
+let board = [
+  [0, 0, 0, 0]
+  [0, 0, 0, 0]
+  [0, 0, 0, 0]
+  [0, 0, 0, 0]
+]
+
+
+//Functions
+// Init 
+// addtile function which i can use as callback for init function and call once after 
+// createBoard maybe create html element and push it to Array or set variable to array
+// directional move function that checks the rows squares with logic, squares will square 
+//event listener for keyup arrows
+// game over function
+// restart function
+
+
+
+
+//potenially using switch statements for keyupsaddevent listener //https://www.freecodecamp.org/news/javascript-switch-case-js-switch-statement-example/

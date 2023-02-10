e9a05137e4b94631ad0c37de410fa3a942fb74e7
Tyler Carter <113203952+bojeebs@users.noreply.github.com>
2023-02-09 20:09:41 -0600

    Comments
    added comments for presentation

--- script.js
+++ script.js
@@ -24,10 +24,14 @@ let board = [
  //<<<got this idea from Kirstina to id each square to help with game state https://stackoverflow.com/questions/9422974/createelement-with-id
 // this function loops through the array creates a div sets the id to = the row and column from the loop and then display that via textcontent.
 
-//Things I learned about init() and setSquareClass()...calling the callback function and textcontent before the append. console log after everystep to isolate the issue.
-//big problem that took forever was that i need to actually add class from css to new squares BEFORE they are appended so the numbers are styled
-//and actually show up in the html. 0's appeared on the board so at the tail end of setSquaresClass () theres an if statement to set the value of the textcontent
-//to empty string if value is 0 which basically hides the zeros so only 2+ shows on the board after we invoke our addSquare().
+
+
+// ! calling the callback function and textcontent before the append. console log after everystep to isolate the issue.
+// ! big problem that took forever was that i need to actually add class from css to new squares BEFORE they are appended so the numbers are styled
+// ! and actually show up in the html. 0's appeared on the board so at the tail end of setSquaresClass () theres an if statement to set the value of the textcontent
+// ! to empty string if value is 0 which basically hides the zeros so only 2+ shows on the board after we invoke our addSquare().
+
+
 function init () {
   let boardEl = document.getElementById("board")
   for (let r = 0; r < board.length; r++) {
@@ -45,32 +49,9 @@ addSquare()
 }
 
 
-
-// function newGame() {
-//   board = [];
-//   for (let r = 0; r < 4; r++) {
-//     let row = [];
-//     for (let c = 0; c < 4; c++) {
-//       
-     
-//       let square = document.getElementById(`square-${r}-${c}`)
-//       square.textContent = board[r][c]
-//       setSquareClass(square, r, c)
-//     }
-    
-//   }
-
-//   addSquare()
-//   addSquare()
-// }
-
-
-
-
-
-
-// i have the function of setSquareClass that callsback and sets the class for all newly created squares and removes 0's from displaying.
-//Will add color classes to this later if theres enough time 
+// ! i have the function of setSquareClass that callsback and sets the class for all newly created squares and removes 0's from displaying.
+// ! Will add color classes to this later if theres enough time 
+// ! Spread operator spreads elements from class list into individual arguments so each class is passed separtely through .remove method
 function setSquareClass(square, r, c) {
   square.className = ''
   square.classList.remove(...square.classList)
@@ -82,9 +63,9 @@ function setSquareClass(square, r, c) {
   }
 }
 
-//This function adds a square with value of 2 randomly. It does this by looping through the array and storing all rows and columns with a 0 value in 
-//the blankSquare array so only cells with value will be considered for selection. Then a simple if statment does random math * the current length
-// of the blankSquare array and stores it in a variable. the r and c values are then extracted from the selected random square and square to a value of 2. 
+// ! This function adds a square with value of 2 randomly. It does this by looping through the array and storing all rows and columns with a 0 value in 
+// ! the blankSquare array so only cells with value will be considered for selection. Then a simple if statment does random math * the current length
+// ! of the blankSquare array and stores it in a variable. the r and c values are then extracted from the selected random square and square to a value of 2. 
 function addSquare() {
   let blankSquare = [];
     for (let r = 0; r < board.length; r++) {
@@ -106,8 +87,6 @@ function addSquare() {
     }
   }
   
-// else if(blankSquare.length === 0) {
-//   console.log("game is over")
 
   //else if for adding 2 random squares if all tiles open or stopping if no tiles open or adding only 1 tile
 //potenially using switch statements for keyupsaddevent listener 
@@ -145,132 +124,153 @@ document.onkeydown = function(event) {
 //based upon what ive looked up your effectively creating a new array and updating the new array to handle the filter zero issue.
 //so creating several variables for handling the new array and using forEach to iterate over those. 
 
-//basically copying whats up above on line 65. 
+// ! So this is a mess effectively its supposed to loop through the board and a series of if statements would check cells to left or right or up/down 
+// ! it should store all the not 0's in the newBoard array and the for loops will loop through and all merges get matched up.
+// ! while loop goes through board and pushes zeros to new array
+// ! set old array to new array and then loop through and id each index or set empty string to 0's so they dont display
+// ! What I would of done different is break this function into 3 functions with very specific purposes and then pass in those functions with arguments that match 
+// ! both functions. So consistency of method through code blocks seems to help the readability and funcitonailty. 
 
-// function moveLeft () {
-//   for (let r = 0; r < board.length; r++) {
-//     let newBoard = []
-//     for (let c = 0; c < board[r].length; c++) {
-//       let square = board[r][c]
-//       console.log("🚀 ~ file: script.js:128 ~ moveLeft ~ square", square)     
-//       if (square !== 0) {
-//         newBoard.push(square)
-//         } if (board[r][c] === board[r][c-1] && board[r][c] !== 0)  {
-//         newBoard[c] *= 2
+function moveLeft () {
+  for (let r = 0; r < board.length; r++) {
+    let newBoard = []
+    for (let c = 0; c < board[r].length; c++) {
+      let square = board[r][c]
+      console.log("🚀 ~ file: script.js:128 ~ moveLeft ~ square", square)     
+      if (square !== 0) {
+        newBoard.push(square)
+        } if (board[r][c] === board[r][c-1] && board[r][c] !== 0)  {
+        newBoard[c] *= 2
     
-//       newBoard[c-1] = 0
-//       }
-//     }
-//     while (newBoard.length < 4) {
-//       newBoard.push(0)
-//     }
-//     board[r] = newBoard
-//     for (let c = 0; c < newBoard.length; c++) {
-//       let square = document.getElementById(`square-${r}-${c}`)
-//       if (newBoard[c] === 0) {
-//         square.textContent = ""
-//     } else {
-//       square.classList.remove('hidden')
-//       square.textContent = newBoard[c]
-//     }
-//    }
-//   } 
-//  }
+      newBoard[c-1] = 0
+      }
+    }
+    while (newBoard.length < 4) {
+      newBoard.push(0)
+    }
+    board[r] = newBoard
+    for (let c = 0; c < newBoard.length; c++) {
+      let square = document.getElementById(`square-${r}-${c}`)
+      if (newBoard[c] === 0) {
+        square.textContent = ""
+    } else {
+      square.classList.remove('hidden')
+      square.textContent = newBoard[c]
+    }
+   }
+  } 
+ }
 
-//  function moveRight () {
-//   for (let r = 0; r < board.length; r++) {
-//     let newBoard = []
-//     for (let c = board[r].length - 1; c >= 0; c--) {
-//       let square = board[r][c]   
-//       if (square !== 0) {
-//         newBoard.push(square)
-//       } if (board[r][c] === board[r][c+1] && board[r][c] !== 0)  {
-//         newBoard[newBoard.length - 1] *= 2
-//       // const mergedSquare = newBoard[newBoard.length-1] *= 2;
-//       // while (newBoard[c+1] === 0) {
-//       //   newBoard[c+1] = mergedSquare
-//       // }
-//       newBoard[c+1] = 0
-//       }
+ function moveRight () {
+  for (let r = 0; r < board.length; r++) {
+    let newBoard = []
+    for (let c = board[r].length - 1; c >= 0; c--) {
+      let square = board[r][c]   
+      if (square !== 0) {
+        newBoard.push(square)
+      } if (board[r][c] === board[r][c+1] && board[r][c] !== 0)  {
+        newBoard[newBoard.length - 1] *= 2
+      // const mergedSquare = newBoard[newBoard.length-1] *= 2;
+      // while (newBoard[c+1] === 0) {
+      //   newBoard[c+1] = mergedSquare
+      // }
+      newBoard[c+1] = 0
+      }
     
-//   }
-//     while (newBoard.length < 4) {
-//       newBoard.push(0)
-//     }
+  }
+    while (newBoard.length < 4) {
+      newBoard.push(0)
+    }
     
-//     board[r] = newBoard
-//     for (let c = 0; c < newBoard.length; c++) {
-//       let square = document.getElementById(`square-${r}-${c}`)
-//       if (newBoard[c] === 0) {
-//         square.textContent = ""
-//     } else {
-//      square.textContent = newBoard[c]
-//     }
-//    }
-//   } 
-//  }
+    board[r] = newBoard
+    for (let c = 0; c < newBoard.length; c++) {
+      let square = document.getElementById(`square-${r}-${c}`)
+      if (newBoard[c] === 0) {
+        square.textContent = ""
+    } else {
+     square.textContent = newBoard[c]
+    }
+   }
+  } 
+ }
 
 
-// function moveUp() {
-//   for (let c = 0; c < board.length; c++) {
-//     let newBoard = []
-//     for (let r = 0; r < board[c].length; r++) {
-//       let square = board[r][c]
-//       if (square !== 0) {
-//         newBoard.push(square)
-//       }
+function moveUp() {
+  for (let c = 0; c < board.length; c++) {
+    let newBoard = []
+    for (let r = 0; r < board[c].length; r++) {
+      let square = board[r][c]
+      if (square !== 0) {
+        newBoard.push(square)
+      }
       
-//       if (board[r][c] === board[r-1 > -1 ? r-1 :r][c] && board[r][c] !== 0) {
-//         newBoard[newBoard.length-1] *= 2
-//         newBoard[r-1 > -1 ? r-1 :r][c] = 0
-//       }
-//     }
-//     while (newBoard.length < 4) {
-//       newBoard.push(0)
-//     }
-//     for (let r = 0; r < newBoard.length; r++) {
-//       board[r][c] = newBoard[r]
+      if (board[r][c] === board[r-1 > -1 ? r-1 :r][c] && board[r][c] !== 0) {
+        newBoard[newBoard.length-1] *= 2
+        newBoard[r-1 > -1 ? r-1 :r][c] = 0
+      }
+    }
+    while (newBoard.length < 4) {
+      newBoard.push(0)
+    }
+    for (let r = 0; r < newBoard.length; r++) {
+      board[r][c] = newBoard[r]
+      let square = document.getElementById(`square-${r}-${c}`)
+      if (newBoard[r] === 0) {
+        square.textContent = ""
+      } else {
+        square.textContent = newBoard[r]
+      }
+    }
+  }
+}
+
+function moveDown() {
+  for (let c = 0; c < board.length; c++) {
+    let newBoard = []
+    for (let r = 0; r < board[c].length; r++) {
+      let square = board[r][c]
+      if (square !== 0) {
+        newBoard.push(square)
+      }
+      if (board[r][c] === board[r -1][c] && board[r][c] !== 0) {
+        newBoard[newBoard.length -1] *= 2
+        newBoard[r][c - 1] = 0
+      }
+    }
+    while (newBoard.length < 4) {
+      newBoard.push(0)
+    }
+    for (let r = 0; r < newBoard.length; r++) {
+      board[r][c] = newBoard[r]
+      let square = document.getElementById(`square-${r}-${c}`)
+      if (newBoard[r] === 0) {
+        square.textContent = ""
+      } else {
+        square.textContent = newBoard[r]
+      }
+    }
+  }
+}
+
+
+
+// function newGame() {
+//   board = [];
+//   for (let r = 0; r < 4; r++) {
+//     let row = [];
+//     for (let c = 0; c < 4; c++) {
+//       
+     
 //       let square = document.getElementById(`square-${r}-${c}`)
-//       if (newBoard[r] === 0) {
-//         square.textContent = ""
-//       } else {
-//         square.textContent = newBoard[r]
-//       }
+//       square.textContent = board[r][c]
+//       setSquareClass(square, r, c)
 //     }
+    
 //   }
+
+//   addSquare()
+//   addSquare()
 // }
-
-// function moveDown() {
-//   for (let c = 0; c < board.length; c++) {
-//     let newBoard = []
-//     for (let r = 0; r < board[c].length; r++) {
-//       let square = board[r][c]
-//       if (square !== 0) {
-//         newBoard.push(square)
-//       }
-//       if (board[r][c] === board[r -1][c] && board[r][c] !== 0) {
-//         newBoard[newBoard.length -1] *= 2
-//         newBoard[r][c - 1] = 0
-//       }
-//     }
-//     while (newBoard.length < 4) {
-//       newBoard.push(0)
-//     }
-//     for (let r = 0; r < newBoard.length; r++) {
-//       board[r][c] = newBoard[r]
-//       let square = document.getElementById(`square-${r}-${c}`)
-//       if (newBoard[r] === 0) {
-//         square.textContent = ""
-//       } else {
-//         square.textContent = newBoard[r]
-//       }
-//     }
-//   }
-// }
-
-
-
-
 
 
 
